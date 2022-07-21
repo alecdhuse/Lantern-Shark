c36358006a0a5dea7a06ca1386d893564300f6c0
Alec Dhuse <alecdhuse@gmail.com>
2022-07-21 14:37:54 -0400

    Added base threat identification object for future use.

---
+++ js/identification_db.js
@@ -0,0 +1,18 @@
+var threat_identification = {
+  'lnk': [
+    {
+      'file_format': "lnk",
+      'creation_date': "2021-08-13T17:12:20.726Z",
+      'last_saved_location': "C:\\Users\\lamar\\Desktop",
+      'identification': "IcedID"
+    }
+  ],
+  'xls': [
+    {
+      'file_format': "xls",
+      'author': "Dream",
+      'creation_date': "2015-06-05T18:19:34.000Z",
+      'identification': "Emotet Dropper"
+    }
+  ]
+};
