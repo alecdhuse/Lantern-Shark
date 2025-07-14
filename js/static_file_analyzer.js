ed64b6c16d85c507d3f45ae6bb5dc52f45cd3395
Alec Dhuse <alecdhuse@gmail.com>
2025-07-14 15:57:21 -0400

    Add better error handling

--- js/static_file_analyzer.js
+++ js/static_file_analyzer.js
@@ -11631,23 +11631,27 @@ class PDF_Parser {
    * @return {Object}   The dictionary object for the trailer.
    */
   static async get_pdf_trailer(file_text) {
-    let trailer_start_regex = /trailer\s*<</gmi;
-    let trailer_start_match = trailer_start_regex.exec(file_text);
-    let trailer_end_index = file_text.indexOf(">>", trailer_start_match.index);
-    let trailer_text = file_text.substring(trailer_start_match.index + trailer_start_match[0].length, trailer_end_index);
-    let parsed_object_dict = await PDF_Parser.get_object_dictionary_values(trailer_text);
+    try {
+      let trailer_start_regex = /trailer\s*<</gmi;
+      let trailer_start_match = trailer_start_regex.exec(file_text);
+      let trailer_end_index = file_text.indexOf(">>", trailer_start_match.index);
+      let trailer_text = file_text.substring(trailer_start_match.index + trailer_start_match[0].length, trailer_end_index);
+      let parsed_object_dict = await PDF_Parser.get_object_dictionary_values(trailer_text);
 
-    if (parsed_object_dict.hasOwnProperty("ID")) {
-      let ids_as_array = [];
+      if (parsed_object_dict.hasOwnProperty("ID")) {
+        let ids_as_array = [];
 
-      for (let i=0; i<parsed_object_dict['ID'].length; i++) {
-        ids_as_array.push(Uint8Array.fromHex(parsed_object_dict['ID'][i].slice(1,-1)));
+        for (let i=0; i<parsed_object_dict['ID'].length; i++) {
+          ids_as_array.push(Uint8Array.fromHex(parsed_object_dict['ID'][i].slice(1,-1)));
+        }
+
+        parsed_object_dict['ID'] = ids_as_array;
       }
 
-      parsed_object_dict['ID'] = ids_as_array;
+      return parsed_object_dict;
+    } catch(err) {
+      return {};
     }
-
-    return parsed_object_dict;
   }
 
 }
